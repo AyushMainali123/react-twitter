742471f9c85c1e3887265aba6dd82979c2ec7473
AyushMainali123 <ayushmainali11@gmail.com>
2021-05-11 16:56:01 +0545

    Made basic twitter landing page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import TwitterLogoSVG from "@components/SVGs/TwitterLogoSVG";
 import { breakpoints } from "@styles/breakpoints";
 import { colors } from "@styles/colors";
+import Button from "@components/Button";
 
 const ContainerGrid = styled.div`
   display: grid;
@@ -11,18 +12,18 @@ const ContainerGrid = styled.div`
     color: ${colors.white};
     background: ${colors.black};
   }
+  min-height: 100vh;
 `;
 
-const GridLeft = styled.div``;
-
-const GridRight = styled.div`
+const GridLeft = styled.div`
+  background: ${colors.primary};
   display: grid;
   place-items: center;
 `;
 
-const SVGLogoLeft = styled(TwitterLogoSVG)`
-  background: ${colors.primary};
-  fill: ${colors.white};
+const GridRight = styled.div`
+  display: grid;
+  place-items: center;
 `;
 
 const SVGLogoRight = styled(TwitterLogoSVG)`
@@ -34,36 +35,46 @@ const GridRightWrapper = styled.div`
   width: 95%;
   margin: auto;
   padding: 15px;
+`;
+const SVGLogoLeft = styled(TwitterLogoSVG)`
+  fill: ${colors.white};
+  max-height: 380px;
+  width: 95%;
+`;
 
-`
+const GridLeftWrapper = styled.div`
+`;
 
 const H1 = styled.h1`
   font-size: 80px;
   letter-spacing: 0px;
   margin: 48px 0;
-`
+`;
 
-const H2 =  styled.h2`
+const H2 = styled.h2`
   margin-top: 20px;
   margin-bottom: 32px;
   font-size: 35px;
-`
+`;
+
+const StyledButton = styled(Button)`
+  padding: 15px 100px;
+`;
 
 export default function Home() {
   return (
     <ContainerGrid>
       <GridLeft>
-        <SVGLogoLeft />
+        <GridLeftWrapper>
+          <SVGLogoLeft />
+        </GridLeftWrapper>
       </GridLeft>
       <GridRight>
         <GridRightWrapper>
           <SVGLogoRight />
-          <H1>
-            Happening Now
-          </H1>
-          <H2>
-            Join Twitter today.
-          </H2>
+          <H1>Happening Now</H1>
+          <H2>Join Twitter today.</H2>
+          <StyledButton>Log In With Twitter</StyledButton>
         </GridRightWrapper>
       </GridRight>
     </ContainerGrid>
